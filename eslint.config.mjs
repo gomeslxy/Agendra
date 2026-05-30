d469408ee1d51817a263db23cd5f3e5c2a8c67d4
Lucas Gomes <la181009@gmail.com>
2026-05-29 22:07:57 -0300

    chore: add obsidian directory to eslint ignore list

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,7 +21,7 @@ const eslintConfig = [
     },
   },
   {
-    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts", "scratch/**", "scripts/**"],
+    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts", "scratch/**", "scripts/**", "obsidian/**"],
   },
 ];
 
